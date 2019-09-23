776e882971445a171ab2ef12662715ce9ce19daf
BuildTools <reece.tait04@gmail.com>
2019-09-23 10:30:50 +0100

    Added in erb files and css

--- wadapp.rb
+++ wadapp.rb
@@ -23,43 +23,18 @@ get '/' do
 	len = @info.length
 	len2 = len - 1
 	len3 = len2 - len1
-	@info = @info + len3.to_s
+	@words = len3.to_s
 	@info = info + " " + $myinfo + "!"
 
-	'<html><body>'+
-	'<b>Menu</b><br>'+
-	'<a href="/">Home</a><br>'+
-	'<a href="/create">Create</a><br>'+
-	'<a href="/about">About</a><br>'+
-	'<a href="/edit">Edit</a><br>'+
-	'<br><br>'+ @info +
-	'</body></html>'
+	erb :home
 end
 
 get '/about' do
-	'<html><body>'+
-	'<b>Menu</b><br>'+
-	'<a href="/">Home</a><br>'+
-	'<a href="/create">Create</a><br>'+
-	'<a href="/about">About</a><br>'+
-	'<a href="/edit">Edit</a><br>'+
-	'<br><br>'+
-	'<h2>About Us</h2>' +
-	'<p>This wiki was created by </p>' + $myinfo + '<p>Staff ID: XXXXXXXX</p>'+
-	'</body></html>'
+	erb :about
 end
 
 get '/create' do
-	'<html><body>'+
-	'<b>Menu</b><br>'+
-	'<a href="/">Home</a><br>'+
-	'<a href="/create">Create</a><br>'+
-	'<a href="/about">About</a><br>'+
-	'<a href="/edit">Edit</a><br>'+
-	'<br><br>'+
-	'<h2>This is your own personal create page!</h2>' +
-	'<section id="add">' + $myinfo + '</section>'+
-	'</body></html>'
+	erb :create
 end
 
 get '/edit' do
@@ -71,13 +46,7 @@ get '/edit' do
 	file.close
 	@info = info
 
-	'<h2>Edit Article</h2><br>'+
-	'<form action="/edit" method="post" id="edit">
-		<input type="hidden" name="_method" value="put">
-		<textarea rows="20" cols="90" name="message">' + @info + '</textarea>
-		<input type="submit" value="Update">
-	</form>'+
-	'<p><a href="../reset">Reset</a></p>'
+	erb :edit
 end
 
 put '/edit' do
